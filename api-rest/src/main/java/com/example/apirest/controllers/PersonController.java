1f99616b9de9117f474afaf90b07023dc76beabe
Araya Alejo <103607905+Araya-Alejo@users.noreply.github.com>
2022-09-02 11:31:27 -0300

    generation of generic controllers

--- api-rest/src/main/java/com/example/apirest/controllers/PersonController.java
+++ api-rest/src/main/java/com/example/apirest/controllers/PersonController.java
@@ -10,53 +10,5 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin( origins = "*" )
 @RequestMapping( path = "api/v1/persons" )
-public class PersonController {
-
-	@Autowired
-	private PersonServiceImpl personServiceImpl;
-
-	@GetMapping( "" )
-	public ResponseEntity<?> getAll ( ) {
-		try {
-			return ResponseEntity.status( HttpStatus.OK).body( personServiceImpl.findAll());
-		}catch ( Exception e ) {
-			return ResponseEntity.status( HttpStatus.NOT_FOUND ).body( "\"error\":\"Error. Por gavot intente mas tarde.\"}" );
-		}
-	}
-
-	@GetMapping("/{id}")
-	public ResponseEntity<?> getOne( @PathVariable Long id ){
-		try {
-			return ResponseEntity.status( HttpStatus.OK).body( personServiceImpl.findById( id ));
-		}catch ( Exception e ) {
-			return ResponseEntity.status( HttpStatus.NOT_FOUND ).body( "\"error\":\"Error. Por gavot intente mas tarde.\"}" );
-		}
-	}
-
-	@PostMapping("")
-	public ResponseEntity<?> save(@RequestBody Person entity){
-		try {
-			return ResponseEntity.status( HttpStatus.OK).body( personServiceImpl.save( entity ));
-		}catch ( Exception e ) {
-			return ResponseEntity.status( HttpStatus.BAD_REQUEST ).body( "\"error\":\"Error. Por gavot intente mas tarde.\"}" );
-		}
-	}
-
-	@PutMapping("/{id}")
-	public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Person entity){
-		try {
-			return ResponseEntity.status( HttpStatus.OK).body( personServiceImpl.update( id,entity ));
-		}catch ( Exception e ) {
-			return ResponseEntity.status( HttpStatus.BAD_REQUEST ).body( "\"error\":\"Error. Por gavot intente mas tarde.\"}" );
-		}
-	}
-
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> delete(@PathVariable Long id){
-		try {
-			return ResponseEntity.status( HttpStatus.NO_CONTENT).body( personServiceImpl.delete( id ));
-		}catch ( Exception e ) {
-			return ResponseEntity.status( HttpStatus.BAD_REQUEST ).body( "\"error\":\"Error. Por gavot intente mas tarde.\"}" );
-		}
-	}
+public class PersonController extends BaseControllerImpl<Person, PersonServiceImpl>{
 }
